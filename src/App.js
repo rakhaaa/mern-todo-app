239695828f57be0a29cbcab88f94c9f38e9dfea8
rakhaaa <rakhaalifalmay25@gmail.com>
2022-11-10 14:40:56 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateToDo from "./components/create-todo";
+import EditToDo from "./components/edit-todo";
+import ToDosList from "./components/todos-list";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">
+              MERN-stack Todo App
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Routes>
+            <Route path="/" exact element={<ToDosList />} />
+            <Route path="/create" exact element={<CreateToDo />} />
+            <Route path="/edit/:id" exact element={<EditToDo />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
